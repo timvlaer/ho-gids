580567331d609c2665630084cb29736068cdae34
Tim Van Laer <timvanlaer@gmail.com>
2014-07-10 23:39:08 +0200

    styles added to map

--- app/scripts/controllers/kaart.js
+++ app/scripts/controllers/kaart.js
@@ -8,7 +8,7 @@
  * Controller of the hoGidsApp
  */
 angular.module('hoGidsApp')
-  .controller('KaartCtrl', function ($scope) {
+  .controller('KaartCtrl', function ($scope, $http) {
 
 	angular.extend($scope, {
 	    center: {
@@ -17,5 +17,63 @@ angular.module('hoGidsApp')
 	        zoom: 14
 	    }
 	});
+
+	var styles = {
+		'pavilioen': {
+			fillColor: 'yellow',
+			fillOpacity: 0.7,
+            weight: 0          
+		},
+		'loods': {
+			fillColor: 'yellow',
+			fillOpacity: 0.7,
+            weight: 0          
+		},
+		'kampeergrond': {
+			fillColor: '#c40000',
+			fillOpacity: 0.7,
+            weight: 0          
+		},
+		'weg-hard': {
+            weight: 4,
+            opacity: 1,
+            color: 'white'
+        },
+        'weg-halfhard': {
+            weight: 4,
+            opacity: 1,
+            dashArray: '5',
+            color: 'white'
+        },
+		'weg-zand': {
+            weight: 2,
+            opacity: 1,
+            color: 'white',
+            dashArray: '3'
+        },
+        'default': {
+			fillColor: 'black',
+            weight: 1,
+            opacity: 1,
+            color: 'white',
+            fillOpacity: 0.7
+		}
+	};
+
+	function style(feature) {
+		console.log(feature.properties.style)
+		var style  = styles[feature.properties.style];
+		return style || styles.default;
+    };
+
+	$http.get("data/map.geojson")
+		.success(function(data, status) {
+			angular.extend($scope, {
+				geojson: {
+					data: data,
+					style: style
+				}
+			});
+		});
 
   });
