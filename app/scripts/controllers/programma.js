9a367c43559d6b52c49086a233f5709c811a8643
Tim Van Laer <tim.vanlaer@ipc.be>
2016-01-31 20:10:10 +0100

    include fonts

--- app/scripts/controllers/programma.js
+++ app/scripts/controllers/programma.js
@@ -8,20 +8,24 @@
  * Controller of the hoGidsApp
  */
 angular.module('hoGidsApp')
-  .controller('ProgrammaCtrl', function ($scope, $location, Programma, localStorageService) {
-  	$scope.programma = Programma.programma;
+    .controller('ProgrammaCtrl', function ($scope, $location, Programma, localStorageService) {
+        $scope.programma = Programma.programma;
 
-    $scope.gouw = localStorageService.get('gouw');
+        $scope.gouw = localStorageService.get('gouw');
 
-  	$scope.toonOpKaart = function(item) {
-  		if(item.plaats) {
-  			item.selected = true;
-        if(item.plaats.toLowerCase().indexOf('gouw') >= 0) {
-          $location.path('/kaart/' + $scope.gouw.grond);
-        } else {
-          $location.path('/kaart/' + item.plaats);
-        }
-  		}
-  	};
+        $scope.toonOpKaart = function (item) {
+            if (item.plaats) {
+                item.selected = true;
+                if (item.plaats.toLowerCase().indexOf('gouw') >= 0) {
+                    if($scope.gouw) {
+                        $location.path('/kaart/' + $scope.gouw.grond);
+                    } else {
+                        //TODO toon melding 'je stelde geen gouw in' + ga naar instellingen om je gouw in te stellen?
+                    }
+                } else {
+                    $location.path('/kaart/' + item.plaats);
+                }
+            }
+        };
 
-});
+    });
