37b6d4860d80f0616255000d7c4dbaa4d38677fd
Tim Van Laer <timvanlaer@gmail.com>
2016-06-23 20:24:28 +0200

    programma update

--- app/scripts/services/programma.js
+++ app/scripts/services/programma.js
@@ -167,6 +167,8 @@ angular.module('hoGidsApp')
 		          'beschrijving': 'Opstaan en ontbijt op je gouwgrond', 'plaats': 'Gouwgrond' },
                 { 'start': '09:00', 'stop': '11:00', 'titel': 'Gouw- en districtsmoment',
                     'plaats': 'Gouwgrond' },
+                { 'start': '11:15', 'stop': '11:45', 'titel': 'Opening van de dag',
+                    'plaats': 'Gouwgrond' },
                 { 'start': '11:45', 'stop': '12:30', 'titel': 'Gouwbattle',
                     'plaats': 'Gouwgrond' },
                 { 'start': '12:30', 'stop': '13:30', 'titel': 'Picknick',
