ae1645c4ac8b8f9b9b492dda7359c81cc97446e3
Tim Van Laer <timvanlaer@gmail.com>
2016-08-02 16:16:39 +0200

    open in system browser

--- app/scripts/controllers/over.js
+++ app/scripts/controllers/over.js
@@ -5,13 +5,8 @@ angular.module('hoGidsApp')
 
     var GITHUB_URL = 'https://github.com/timvlaer/ho-gids';
 
-    $scope.showGithubPage = function(e) {
-      if (typeof cordova !== 'undefined') {
-        cordova.InAppBrowser.open(GITHUB_URL, '_system', 'location=yes');
-      } else {
-        window.open(GITHUB_URL, '_system', 'location=yes');
-      }
-      e.preventDefault();
+    $scope.showGithubPage = function() {
+      window.open(GITHUB_URL, '_system');
     }
 
   });
