56844991bff09a6c7e8260d44fdb7859bd9e2df0
Tim Van Laer <timvanlaer@gmail.com>
2016-08-03 13:29:02 +0200

    removed cordova specific js code

--- app/scripts/controllers/over.js
+++ app/scripts/controllers/over.js
@@ -6,7 +6,7 @@ angular.module('hoGidsApp')
     var GITHUB_URL = 'https://github.com/timvlaer/ho-gids';
 
     $scope.showGithubPage = function() {
-      window.open(GITHUB_URL, "_blank");
+      window.open(GITHUB_URL, "_system");
     }
 
   });
