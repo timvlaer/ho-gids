8fae1103674bda9f81f26917d4a48aa769faf5c5
Tim Van Laer <timvanlaer@gmail.com>
2016-08-03 23:40:28 +0200

    added social media links #24

--- app/scripts/controllers/menu.js
+++ app/scripts/controllers/menu.js
@@ -10,6 +10,10 @@
 angular.module('hoGidsApp')
   .controller('MenuCtrl', function ($scope, $location, localStorageService) {
 
+    var TWITTER_URL = "https://twitter.com/ScoutsGidsenVL",
+      FB_URL = "https://www.facebook.com/scoutsengidsenvlaanderen/",
+      INSTAGRAM_URL = "https://www.instagram.com/scoutsgidsenvl/";
+
     $scope.gouw = localStorageService.get('gouw');
 
   	$scope.isActive = function (viewLocation) {
@@ -18,6 +22,10 @@ angular.module('hoGidsApp')
 
     $scope.go = function(location) {
       $location.path(location);
-    }
+    };
+
+    $scope.openInstagramPage = function() { window.open(INSTAGRAM_URL, "_system"); };
+    $scope.openFacebookPage = function() { window.open(FB_URL, "_system"); };
+    $scope.openTwitterPage = function() { window.open(TWITTER_URL, "_system"); };
 
 });
